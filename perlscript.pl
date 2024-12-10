2f3d5e38e80a6f73d63b5d5dd657d371b21e70da
E. Choroba <choroba@matfyz.cz>
2024-12-10 15:23:54 +0100

    Make the perl script nicer

--- perlscript.pl
+++ perlscript.pl
@@ -1,6 +1,7 @@
 #!/usr/bin/perl
 use warnings;
 use strict;
+use feature qw{ say };
 use Config;
 
 local $^I = "";  # In-place edit.
@@ -15,13 +16,13 @@ if ($Config{archname} =~ /^x86_64/) {
 
 @ARGV = 'linux64.h';
 while (<>) {
-    s{/lib/ld-linux.so.2}{/ulinux/lib32/ld-linux.so.2}g;
-    s{/lib64/ld-linux-x86-64.so.2}{/ulinux/lib/ld-linux-x86-64.so.2}g;
-    s{/libx32/ld-linux-x32.so.2}{/ulinux/libx32/ld-linux-x32.so.2}g;
+    s{ (/lib) (/ld-linux\.so\.2)           }{/ulinux${1}32$2}gx;
+    s{ (/lib) 64 (/ld-linux-x86-64\.so\.2) }{/ulinux$1$2}gx;
+    s{ (/libx32/ld-linux-x32\.so\.2)       }{/ulinux$1}gx;
     print;
     if (eof) {
         print "\n";
-        print "#undef STANDARD_STARTFILE_PREFIX_1\n";
-        print qq(#define STANDARD_STARTFILE_PREFIX_1 "/ulinux/lib/"\n);
+        say '#undef STANDARD_STARTFILE_PREFIX_1';
+        say '#define STANDARD_STARTFILE_PREFIX_1 "/ulinux/lib/"';
     }
 }
