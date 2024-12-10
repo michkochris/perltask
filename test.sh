70d35e88fc8c5262f4e627cddabc1e3ec693a377
E. Choroba <choroba@matfyz.cz>
2024-12-10 15:11:11 +0100

    Prepare the test: run perl and compare the resutls

---
+++ test.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+files=(linux64.h t-linux64)
+
+./copy.sh
+./perl.sh
+for f in "${files[@]}" ; do
+    cp $f perl1-$f
+done
+
+./copy.sh
+./perlscript.pl
+for f in "${files[@]}" ; do
+    cp $f perl2-$f
+done
+
+
+./copy.sh
+./sedscript.sh
+for f in "${files[@]}" ; do
+    diff $f perl1-$f
+    diff $f perl2-$f
+done
